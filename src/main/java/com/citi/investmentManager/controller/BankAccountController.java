9816616dad66e2bfe4d8965d935ffb8d59c06dd4
jerquinigo <jonathanerquinigo@pursuit.org>
2021-08-25 17:44:00 +0000

    working springboot routes for bankAccount

--- src/main/java/com/citi/investmentManager/controller/BankAccountController.java
+++ src/main/java/com/citi/investmentManager/controller/BankAccountController.java
@@ -1,2 +1,28 @@
-package com.citi.investmentManager.controller;public class BankAccountController {
+package com.citi.investmentManager.controller;
+
+import com.citi.investmentManager.entities.BankAccount;
+import com.citi.investmentManager.service.BankAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/bankaccount/")
+public class BankAccountController {
+
+    @Autowired
+    private BankAccountService bankAccountService;
+
+    @GetMapping(path = "/allBankAccounts")
+    public Collection<BankAccount> getAllBankAccounts() {
+        return bankAccountService.getAllBankAccounts();
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseBody
+    public Optional<BankAccount> getBankAccountById(@PathVariable Integer id) {
+        return bankAccountService.getBankAccountById(id);
+    }
 }
--- src/main/java/com/citi/investmentManager/entities/BankAccount.java
+++ src/main/java/com/citi/investmentManager/entities/BankAccount.java
@@ -1,2 +1,73 @@
-package com.citi.investmentManager.entities;public class BankAccount {
+package com.citi.investmentManager.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="bankaccount")
+public class BankAccount implements Serializable {
+
+    @Id
+    @Column(name = "bankid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bankId;
+    @Column(name = "bankname")
+    private String bankName;
+    @Column(name = "cashvalue")
+    private Double cashValue;
+    @Column(name = "accounttype")
+    private String accountType;
+    @Column(name = "accountid")
+    private Integer accountId;
+
+    public Integer getBankId() {
+        return bankId;
+    }
+
+    public void setBankId(Integer bankId) {
+        this.bankId = bankId;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public Double getCashValue() {
+        return cashValue;
+    }
+
+    public void setCashValue(Double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Integer getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "bankId=" + bankId +
+                ", bankName='" + bankName + '\'' +
+                ", cashValue=" + cashValue +
+                ", accountType='" + accountType + '\'' +
+                ", accountId=" + accountId +
+                '}';
+    }
 }
