e187d102cac7e42e842099a998fb0f4c0b76dec0
OzunaEddie <ozuna@pop-os.localdomain>
2021-08-27 12:26:26 -0400

    update

--- ui/src/app/net-worth/net-worth.component.ts
+++ ui/src/app/net-worth/net-worth.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, AfterViewChecked } from '@angular/core';
 import { BarChartOption, ChartData, ChartView } from 'ngx-chart';
+import { CookieService } from 'ngx-cookie-service';
 import { multi } from './data';
 
 @Component({
@@ -26,8 +27,12 @@ export class NetWorthComponent  {
   timeline!: boolean 
   maxXAxisTickLength!:number;
   colorScheme!:any;
+  accountOne:any = []
+  accountTwo:any = []
+  accountThree:any = []
+  fractionList:any = []
 
-  constructor() {
+  constructor(private cookieService: CookieService) {
     
   }
 
@@ -40,7 +45,7 @@ export class NetWorthComponent  {
 
     
   }
-
+  
  
   ngOnChanges() {
     this.displayNetWorthChartData()
@@ -49,125 +54,144 @@ export class NetWorthComponent  {
   }
 
   displayNetWorthChartData() {
+
+    for(let i = 0; i < 30; i++){
+     this.accountOne.push(i/1.2)
+     this.accountTwo.push(i/0.90)
+     this.accountThree.push(i/0.50)
+    }
+
+    if(this.cookieService.get( 'id') == "1"){
+      this.fractionList = this.accountOne
+    }
+    
+    if(this.cookieService.get( 'id') == "2"){
+      this.fractionList = this.accountTwo
+    }
+
+    if(this.cookieService.get( 'id') == "3"){
+      this.fractionList = this.accountThree
+    }
+
     this.multi = [
       {
         "name": "netWorth",
         "series": [
           {
             "name": "1",
-            "value": (this.netWorth/8.75)
+            "value": (this.netWorth/this.fractionList[29])
           },
           {
             "name": "2",
-            "value": (this.netWorth/8.50)
+            "value": (this.netWorth/this.fractionList[28])
           },
           {
             "name": "3",
-            "value": (this.netWorth/8.25)
+            "value": (this.netWorth/this.fractionList[27])
           },
           {
             "name": "4",
-            "value": (this.netWorth/8.15)
+            "value": (this.netWorth/this.fractionList[26])
           },
           {
             "name": "5",
-            "value": (this.netWorth/8.00)
+            "value": (this.netWorth/this.fractionList[25])
           },
           {
             "name": "6",
-            "value": (this.netWorth/7.66)
+            "value": (this.netWorth/this.fractionList[24])
           },
           {
             "name": "7",
-            "value": (this.netWorth/7.45)
+            "value": (this.netWorth/this.fractionList[23])
           },
           {
             "name": "8",
-            "value": (this.netWorth/7.37)
+            "value": (this.netWorth/this.fractionList[22])
           },
           {
             "name": "9",
-            "value": (this.netWorth/7.10)
+            "value": (this.netWorth/this.fractionList[21])
           },
           {
             "name": "10",
-            "value": (this.netWorth/6.47)
+            "value": (this.netWorth/this.fractionList[20])
           },
           {
             "name": "11",
-            "value": (this.netWorth/6.36)
+            "value": (this.netWorth/this.fractionList[19])
           },
           {
             "name": "12",
-            "value": (this.netWorth/6.10)
+            "value": (this.netWorth/this.fractionList[18])
           },
           {
             "name": "13",
-            "value": (this.netWorth/5.48)
+            "value": (this.netWorth/this.fractionList[17])
           },
           {
             "name": "14",
-            "value": (this.netWorth/5.22)
+            "value": (this.netWorth/this.fractionList[16])
           },
           {
             "name": "15",
-            "value": (this.netWorth/4.90)
+            "value": (this.netWorth/this.fractionList[15])
           },
           {
             "name": "16",
-            "value": (this.netWorth/4.20)
+            "value": (this.netWorth/this.fractionList[14])
           },
           {
             "name": "17",
-            "value": (this.netWorth/4.10)
+            "value": (this.netWorth/this.fractionList[13])
           },
           {
             "name": "18",
-            "value": (this.netWorth/3.60)
+            "value": (this.netWorth/this.fractionList[12])
           },
           {
             "name": "19",
-            "value": (this.netWorth/3.10)
+            "value": (this.netWorth/this.fractionList[11])
           },
           {
             "name": "20",
-            "value": (this.netWorth/3.00)
+            "value": (this.netWorth/this.fractionList[10])
           },
           {
             "name": "21",
-            "value": (this.netWorth/3.60)
+            "value": (this.netWorth/this.fractionList[9])
           },
           {
             "name": "22",
-            "value": (this.netWorth/3.70)
+            "value": (this.netWorth/this.fractionList[8])
           },
           {
             "name": "23",
-            "value": (this.netWorth/3.90)
+            "value": (this.netWorth/this.fractionList[7])
           },
           {
             "name": "24",
-            "value": (this.netWorth/2.50)
+            "value": (this.netWorth/this.fractionList[6])
           },
           {
             "name": "25",
-            "value": (this.netWorth/2.10)
+            "value": (this.netWorth/this.fractionList[5])
           },
           {
             "name": "26",
-            "value": (this.netWorth/2.00)
+            "value": (this.netWorth/this.fractionList[4])
           },
           {
             "name": "27",
-            "value": (this.netWorth/1.50)
+            "value": (this.netWorth/this.fractionList[3])
           },
           {
             "name": "28",
-            "value": (this.netWorth/1.20)
+            "value": (this.netWorth/this.fractionList[2])
           },
           {
             "name": "29",
-            "value": (this.netWorth/ 1.10)
+            "value": (this.netWorth/ this.fractionList[1])
           },
           {
             "name": "30",
